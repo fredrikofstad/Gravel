3c211b62063297a23e136a2de283e0a05401533a
Fredrik Ofstad <nokotin@gmail.com>
2022-04-04 00:28:03 +0200

    set up premake lua

--- Gravel/src/core/Core.h
+++ Gravel/src/core/Core.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #ifdef GR_PLATFORM_WINDOWS
-	#ifdef GRAVEL_BUILD_DLL
+	#ifdef GR_BUILD_DLL
 		#define GRAVEL_API __declspec(dllexport)
 	#else
 		#define GRAVEL_API __declspec(dllimport)
---
+++ premake5.lua
@@ -0,0 +1,106 @@
+workspace "Gravel"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Distribution"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Gravel"
+	location "Gravel"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include",
+	}
+
+	filter "system:windows"
+		cppdialect "c++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"GR_PLATFORM_WINDOWS",
+			"GR_BUILD_DLL",
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "GR_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "GR_RELEASE"
+		optimize "On"
+
+	filter "configurations:Distribution"
+		defines "GR_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"Gravel/vendor/spdlog/include",
+		"Gravel/src"
+	}
+
+	links
+	{
+		"Gravel"
+	}
+
+	filter "system:windows"
+		cppdialect "c++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"GR_PLATFORM_WINDOWS"
+		}
+
+
+	filter "configurations:Debug"
+		defines "GR_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "GR_RELEASE"
+		optimize "On"
+
+	filter "configurations:Distribution"
+		defines "GR_DIST"
+		optimize "On"
