3979e0ecad43a5e88c0ba20d8e95d97cd32e0e8b
Fredrik Ofstad <nokotin@gmail.com>
2022-07-14 23:59:23 +0200

    Updated content browser with icons

--- Garden/src/Panels/BrowserPanel.cpp
+++ Garden/src/Panels/BrowserPanel.cpp
@@ -11,6 +11,9 @@ namespace Gravel {
 	BrowserPanel::BrowserPanel()
 		: m_currentDirectory(s_resPath)
 	{
+		//consider texture atlas
+		m_folderIcon = Texture2D::Create("res/icons/icon-folder.png");
+		m_fileIcon = Texture2D::Create("res/icons/icon-file.png");
 	}
 
 	void BrowserPanel::OnImGuiRender()
@@ -25,25 +28,40 @@ namespace Gravel {
 			}
 		}
 
+		static float padding = 16.0f;
+		static float thumbnailSize = 50.0f;
+		float cellSize = thumbnailSize + padding;
+
+		float panelWidth = ImGui::GetContentRegionAvail().x;
+		int columnCount = (int)(panelWidth / cellSize);
+		if (columnCount < 1)
+			columnCount = 1;
+
+		ImGui::Columns(columnCount, 0, false);
+
 		for (auto& directoryEntry : std::filesystem::directory_iterator(m_currentDirectory))
 		{
 			const auto& path = directoryEntry.path();
 			auto relativePath = std::filesystem::relative(path, s_resPath);
 			std::string filenameString = relativePath.filename().string();
-			if (directoryEntry.is_directory())
+			Shared<Texture2D> icon = directoryEntry.is_directory() ? m_folderIcon : m_fileIcon;
+			ImGui::ImageButton((ImTextureID)icon->GetRendererID(), { thumbnailSize, thumbnailSize }, { 0, 1 }, { 1, 0 });
+			if (ImGui::IsItemHovered() && ImGui::IsMouseDoubleClicked(ImGuiMouseButton_Left))
 			{
-				if (ImGui::Button(filenameString.c_str()))
-				{
+				if (directoryEntry.is_directory())
 					m_currentDirectory /= path.filename();
-				}
 			}
-			else
-			{
-				if (ImGui::Button(filenameString.c_str()))
-				{
-				}
-			}
+
+			ImGui::TextWrapped(filenameString.c_str());
+
+			ImGui::NextColumn();
 		}
+
+		ImGui::Columns(1);
+
+		ImGui::SliderFloat("Thumbnail Size", &thumbnailSize, 16, 512);
+		ImGui::SliderFloat("Padding", &padding, 0, 32);
+
 
 		ImGui::End();
 	}
--- Garden/src/Panels/BrowserPanel.h
+++ Garden/src/Panels/BrowserPanel.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <filesystem>
+#include "Engine/Renderer/Texture.h"
 
 namespace Gravel {
 
@@ -12,6 +13,9 @@ namespace Gravel {
 		void OnImGuiRender();
 	private:
 		std::filesystem::path m_currentDirectory;
+
+		Shared<Texture2D> m_folderIcon;
+		Shared<Texture2D> m_fileIcon;
 	};
 
 }
